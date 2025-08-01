443ee1437d71d3f4e442b9885c9c426a2be3f204
michael dennis <manullang_d@yahoo.com>
2025-08-01 15:29:26 +0700

    task

--- no2/app/routes/order.routes.js
+++ no2/app/routes/order.routes.js
@@ -9,23 +9,5 @@ module.exports = app => {
   // Create a new order
   router.post("/", Orders.create);
 
-  // Retrieve all Orders
-  router.get("/", Orders.findAll);
-
-  // Retrieve all published Orders
-  router.get("/published", Orders.findAllPublished);
-
-  // Retrieve a single order with id
-  router.get("/:id", Orders.findOne);
-
-  // Update a order with id
-  router.put("/:id", Orders.update);
-
-  // Delete a order with id
-  router.delete("/:id", Orders.delete);
-
-  // Delete all Orders
-  router.delete("/", Orders.deleteAll);
-
   app.use('/api/orders', router);
 };
